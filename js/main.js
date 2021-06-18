9efe1ad5aedb713cea07df2757625cfc4b0fa09b
triplemonster221 <boorder1993@yandex.ru>
2021-06-18 13:48:06 +0300

    added section from[C

---
+++ js/main.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+
+      $('.from__slaider').slick({
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev left"><img src="../img/left1.svg"></button>',
+        nextArrow:'<button type="button" class="slick-prev right"><img src="../img/right.svg"></button>',
+        dots:true
+
+      });
+
+  });
