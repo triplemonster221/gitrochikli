2567c09837532cf74f5019250deba5d68da6298b
triplemonster221 <Boorder1993@yandex.ru>
2021-06-20 16:06:38 +0500

    added section collection

--- js/main.js
+++ js/main.js
@@ -8,4 +8,14 @@
 
       });
 
+      $('.tabs__item').on('click',function(e){
+        e.preventDefault();
+
+        $('.tabs__item').removeClass('tabs__item--active');
+        $(this).addClass('tabs__item--active');
+
+        $('.tabs__content').removeClass('tabs__content--active');
+        $($(this).attr('href')).addClass('tabs__content--active');
+      });
+
   });
